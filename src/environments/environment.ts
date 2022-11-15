8ae318f407300d70ffc6b1140c86e772b7cea6ff
abtripathy <abtripathy.teksystems@gmail.com>
2022-11-15 19:57:40 +0530

    initial setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,27 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // firebase: {
+  //   apiKey: "AIzaSyC-YdjvaLBo4OnRLB4lwMcWnfbrnVkgeCY",
+  //   authDomain: "teksystemspoc-fcm.firebaseapp.com",
+  //   projectId: "teksystemspoc-fcm",
+  //   storageBucket: "teksystemspoc-fcm.appspot.com",
+  //   messagingSenderId: "572894731618",
+  //   appId: "1:572894731618:web:8645ecd7d35ca1a552a03b",
+  //   measurementId: "G-5C1ML732V3",
+  //   vapidKey: "BOhaOiXmW_owfx60iTulZRmXk10iQvtw27qeAEFjnAxXxQXZUCYwNxH0eupU_ckJ3uEBPA5pzW7RFlIwRh_1Xjw"
+  // },
+   firebase: {
+    apiKey: "AIzaSyC5dq6RBLohZgrZXp_rqLFxWgYSd_TDq7g",
+    authDomain: "messaging-poc-project.firebaseapp.com",
+    projectId: "messaging-poc-project",
+    storageBucket: "messaging-poc-project.appspot.com",
+    messagingSenderId: "965576122974",
+    appId: "1:965576122974:web:f495844e6d7a6b09470acf",
+    measurementId: "G-Z49ZT4BS22",
+    vapidKey: "BO-FYANdWygXpNVwJ8RI3qJ7JqPeouHur8Vmo6Bu1gceb6R6UpSoq0JURSn52DVPc-1znQnzDgNwKOyo7fL4c94"
+  },
 };
 
 /*
